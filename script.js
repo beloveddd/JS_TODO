88468bf490415e80c95a9e021cb001eeb0f3362c
Yuliya Bialova <yuliya.bialova@gmail.com>
2022-03-17 20:29:00 +0300

    index.html: add h1; styles.css: create styles for app; script.js: create list of tasks;

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hi");
+const amountOfTasks = 4;
+
+const container = document.querySelector('.container');
+const listOfTasks = document.createElement('ul');
+
+for (let i = 0; i < amountOfTasks; i++) {
+    let task = document.createElement('li');
+    task.innerHTML = `Task ${i+1}`;
+    listOfTasks.append(task);
+}
+
+container.append(listOfTasks);
